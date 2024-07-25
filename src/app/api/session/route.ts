7cf12d1a3d97801e7366ad23c0b2da9b1f9732dd
markosmk <markosmk@me.com>
2024-07-25 11:32:20 -0300

    minor changes

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,11 +1,8 @@
 import { auth } from "@/lib/auth"
 import { NextResponse } from "next/server"
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
   const session = await auth()
-  // return NextResponse.json({
-  //   authenticated: !!session,
-  //   session,
-  // })
+  // return NextResponse.json({ authenticated: !!session, session })
   return NextResponse.json(session ? session : false)
 }
