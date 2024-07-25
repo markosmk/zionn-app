8ebd4436e0a6cea3a07406e22ed064d5d437c17d
markosmk <markosmk@me.com>
2024-07-25 11:22:08 -0300

    update: now using argon

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -1,14 +1,22 @@
-import bcrypt from "bcrypt"
+"use server"
+
+import { hash, verify } from "@node-rs/argon2"
 
 export async function saltAndHashPassword(password: string): Promise<string> {
-  const salt = await bcrypt.genSalt(12)
-  const hash = await bcrypt.hash(password, salt)
-  return hash
+  const pass = await hash(password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  })
+  return pass
 }
 
-export async function verifyPassword(
-  password: string,
-  hash: string
-): Promise<boolean> {
-  return bcrypt.compare(password, hash)
+export async function verifyPassword(hash: string, password: string): Promise<boolean> {
+  return verify(hash, password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  })
 }
